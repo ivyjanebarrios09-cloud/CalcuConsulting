ccf49049412d98f083b5938cd4d6f054ffdeb5e6
Klyxy Crimson <klyxycrimson@gmail.com>
2025-08-25 12:25:44 +0000

    i formatted my .env like this please fix the same error FIREBASE_SERVIC

--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -6,9 +6,8 @@ if (!admin.apps.length) {
     if (!process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
         throw new Error('FIREBASE_SERVICE_ACCOUNT_KEY is not set.');
     }
-    const serviceAccount = JSON.parse(
-        process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
-    );
+    const serviceAccountKey = process.env.FIREBASE_SERVICE_ACCOUNT_KEY.replace(/\\n/g, '\n');
+    const serviceAccount = JSON.parse(serviceAccountKey);
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount)
     });
